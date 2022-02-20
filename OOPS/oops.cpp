73fc59ee65a050c5fb11026237227b253b540762
subodh kumar <70744964+ErSKSinha@users.noreply.github.com>
2022-02-20 23:52:52 +0530

    Create oops.cpp

---
+++ OOPS/oops.cpp
@@ -0,0 +1,18 @@
+/*
+    oops concept all topic as
+      1. object / class
+      2. inheritance
+      3. polymorphisms
+      4. abstraction
+      5. encapsulation
+      
+    other important topic about oops
+    1. Access Specifier : public, protected, private
+    2. scope to access the data member
+    3. scope resolution (::) operator
+    4. constructor
+    5. destructor
+    6. constructor overloading 
+    7. friend function
+    8. operator overloading
+*/
