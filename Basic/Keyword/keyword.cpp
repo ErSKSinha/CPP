5ad6011995544960971b7de86efe04812ec021e8
subodh kumar <70744964+ErSKSinha@users.noreply.github.com>
2022-02-21 00:29:43 +0530

    Create keyword.cpp

---
+++ Basic/Keyword/keyword.cpp
@@ -0,0 +1,41 @@
+/*
+  total keyword : 62 keyword (32 c program keyword + 30 CPP keyword)
+  
+  1. auto   
+  2. break
+  3. case
+  4. for
+  5. const
+  6. continue
+  7. default
+  8. do
+  9. double
+  10. else
+  11. enum
+  12. extern
+  13. float
+  14.  for  
+  15. goto
+  16. if
+  17. int
+  18. long
+  19. register
+  20.  return
+  21.  short
+  22. signed
+  23. sizeof
+  24. static
+  25. struct
+  26. switch
+  27. typedef
+  28. union
+  29. unsigned
+  30. volatile
+  31. void
+  32. while
+  
+  
+  
+  
+ 
+*/
